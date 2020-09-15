fec04ebccbccc8fb756e5441ea1a91c47871df2b
Jon Barson <jbarson@gmail.com>
2020-09-15 17:13:26 -0400

    Props demo complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
+import {v1 as uuidv1} from 'uuid';
+import Header from './components/Header/Header.jsx';
 import './App.css';
 
+
+
+const Teachers = ({teachers}) => {
+  return (
+    <ul>
+      {teachers.map(individual => {
+        return (<li key={individual.id}>{individual.name}, {individual.level}, {individual.id}</li>)
+      })}
+    </ul>
+  )
+}
+
+const NewComponent = () => {
+  const handler = () => console.log('this is in the parent')
+  return (<NewButton clickHandler={handler}/>)
+}
+
+const NewButton = ({clickHandler}) => {
+  const anotherHandler = () => console.log('in the child!')
+  return (
+    <>
+      <button onClick={clickHandler}>click me in the child!</button>
+      <button onClick={anotherHandler}>click me in the child too!</button>
+    </>
+  )
+}
+
+// function Header() {
+//   return (
+//     <h1>Web Devs of the World Unite!</h1>
+//   )
+// }
+
+function Name({firstName, lastName}) {
+  return (
+    <div className="name">
+      <span>{firstName}</span>
+      <span>{lastName}</span>
+    </div>
+  )
+}
+
+
 function App() {
+  const teachers = [
+    {
+      name: "Jon",
+      level: "educator",
+      id: uuidv1()
+    },
+    {
+      name: "Lauren",
+      level: "educator",
+      id: uuidv1()
+    },
+    {
+      name: "Angus",
+      level: "TA",
+      id: uuidv1()
+    },
+    {
+      name: "Jessica",
+      level: "TA",
+      id: uuidv1()
+    }
+  ]
+  const anotherName = {
+    firstName: 'Angus',
+    lastName: 'Ryer'
+  }
+  const educator = {
+    firstName: "Jon",
+    lastName: "Barson"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Name firstName={educator.firstName} lastName={educator.lastName}/>
+      <Name firstName={anotherName.firstName} lastName={anotherName.lastName}/>
+      <NewComponent />
+      <Teachers teachers={teachers}/>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+  return (
+    <h1>Web Devs of the World Unite!</h1>
+  )
+}
+
+export default Header;
