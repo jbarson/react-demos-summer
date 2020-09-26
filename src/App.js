7206f395316b070aae9acbaf4d2c40b4309d8bae
Jon Barson <jbarson@gmail.com>
2020-09-26 12:50:33 -0400

    save storage demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const setStorage = () => {
+    const valToSave = document.getElementById('input').value
+    const jonsObject = {
+      name: 'Jon',
+      value: valToSave
+    }
+    localStorage.setItem('jonsObjectKey', JSON.stringify(jonsObject))
+  }
+  const getStorage = () => {
+    const value = JSON.parse(localStorage.getItem('jonsObjectKey')) 
+    console.log(value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Storage Wars!</h1>
+      <input type="text" id="input" placeholder="data"/>
+      <button onClick={setStorage}>Set Storage</button>
+      <button onClick={getStorage}>Get Storage</button>
     </div>
   );
 }
