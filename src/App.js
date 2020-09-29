c41edcc7b5549814f3d5c3ed36000c2a0bca8eb2
Jon Barson <jbarson@gmail.com>
2020-09-29 14:47:26 -0400

    hooks and other stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    dish: 'class component not set',
+    ingredients: ['still not set']
+  }
+  componentDidMount(){
+     axios.get('http://localhost:8080/famousdish')
+      .then(response => this.setState({dish: response.data}))
+      axios.get('http://localhost:8080/ingredients')
+      .then(response => this.setState({ingredients: response.data}))
+  }
+  // const [dish, setDish] = useState('not set');
+  // const [ingredients, setIngredients] = useState(['not set either']);
+  // useEffect(() => {
+  //   axios.get('http://localhost:8080/famousdish')
+  //     .then(response => setDish(response.data))
+  //     axios.get('http://localhost:8080/ingredients')
+  //     .then(response => setIngredients(response.data))
+  // })
+  render(){
+    return (
+      <div className="App">
+        <p>The Fav Dish is: {this.state.dish}</p>
+        <p>The Ingredients List is: {this.state.ingredients.map(item => <span>{item},</span>)}</p>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
